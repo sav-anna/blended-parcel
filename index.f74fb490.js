3c5fcbcbb7dc0ad7495986471d68548cb8291a86
AnnaVS8 <anna_sibova@ukr.net>
2022-11-19 07:43:35 +0000

    Deploying to gh-pages from @ AnnaVS8/blended-parcel@0d7f44a1c984995e106edeb432d846b14ca3c5cb 🚀

---
+++ index.f74fb490.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden"),document.body.classList.toggle("modal-open")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.f74fb490.js.map
